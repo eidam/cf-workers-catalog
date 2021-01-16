811073a53a93d6b398064615838d03a0c278f309
Adam Janiš <adam.janis@gmail.com>
2021-01-16 18:48:45 +0100

    feat: search index

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,7 +57,6 @@ module.exports = {
                 forkCount
                 stargazerCount
                 name
-                homepageUrl
                 owner {
                   login
                 }
@@ -79,6 +78,84 @@ module.exports = {
         variables: repos,
       },
     },
+    {
+      resolve: "gatsby-plugin-local-search",
+      options: {
+        // A unique name for the search index. This should be descriptive of
+        // what the index contains. This is required.
+        name: "repos",
+
+        // Set the search engine to create the index. This is required.
+        // The following engines are supported: flexsearch, lunr
+        engine: "flexsearch",
+
+        // Provide options to the engine. This is optional and only recommended
+        // for advanced users.
+        //
+        // Note: Only the flexsearch engine supports options.
+        engineOptions: "speed",
+
+        // GraphQL query used to fetch all data for the search index. This is
+        // required.
+        query: `
+          {
+            allGithubData {
+              nodes {
+                id
+                data {
+                  repository {
+                    name
+                    description
+                    forkCount
+                    stargazerCount
+                    owner {
+                      login
+                    }
+                    repositoryTopics {
+                      nodes {
+                        topic {
+                          name
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        `,
+
+        // Field used as the reference value for each document.
+        // Default: 'id'.
+        ref: "id",
+
+        // List of keys to index. The values of the keys are taken from the
+        // normalizer function below.
+        // Default: all fields
+        index: ["name", "owner", "description"],
+
+        // List of keys to store and make available in your UI. The values of
+        // the keys are taken from the normalizer function below.
+        // Default: all fields
+        //store: ['id', 'path', 'title'],
+
+        // Function used to map the result from the GraphQL query. This should
+        // return an array of items to index in the form of flat objects
+        // containing properties to index. The objects must contain the `ref`
+        // field above (default: 'id'). This is required.
+        normalizer: ({ data }) =>
+          data.allGithubData.nodes.map(node => ({
+            id: node.id,
+            owner: node.data.repository.owner.login,
+            name: node.data.repository.name,
+            description: node.data.repository.description,
+            stars: node.data.repository.stargazerCount,
+            forks: node.data.repository.forkCount,
+            topics: node.data.repository.repositoryTopics.nodes,
+          })),
+      },
+    },
+    "gatsby-plugin-use-query-params",
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import React from "react"
-import { Link } from "gatsby"
+import { useFlexSearch } from "react-use-flexsearch"
 import { graphql } from "gatsby"
 import { GridList, GridListTile } from "@material-ui/core"
+import { useQueryParam, StringParam } from "use-query-params"
+import SearchBar from "material-ui-search-bar"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -9,63 +11,55 @@ import SEO from "../components/seo"
 import Item from "../components/item"
 
 export const pageQuery = graphql`
-  query ItemsQuery {
-    allGithubData {
-      nodes {
-        id
-        data {
-          repository {
-            name
-            description
-            homepageUrl
-            forkCount
-            stargazerCount
-            owner {
-              login
-            }
-            repositoryTopics {
-              nodes {
-                topic {
-                  name
-                }
-              }
-            }
-          }
-        }
-      }
+  query ReposQuery {
+    localSearchRepos {
+      index
+      store
     }
   }
 `
 
 const IndexPage = ({ data }) => {
-  const items = []
+  const [query, setQuery] = useQueryParam("search", StringParam)
+  const results = useFlexSearch(
+    query,
+    data.localSearchRepos.index,
+    data.localSearchRepos.store,
+    { suggest: true }
+  )
 
-  for (const item of data.allGithubData.nodes) {
-    console.log(item)
-    items.push(
-      <GridListTile key={item.id} cols={1}>
-        <Item
-          name={item.data.repository.name}
-          description={item.data.repository.description}
-          owner={item.data.repository.owner.login}
-          stars={item.data.repository.stargazerCount}
-          forks={item.data.repository.forkCount}
-          topics={item.data.repository.repositoryTopics.nodes}
-        />
-      </GridListTile>
-    )
-  }
+  const items =
+    query && query.length > 1 ? results : data.localSearchRepos.store
 
   return (
     <Layout>
       <SEO title="Cloudflare Workers catalog" />
+      <SearchBar
+        value={query || undefined}
+        onChange={newValue => setQuery(newValue)}
+      />
+      <br />
       <GridList
         cellHeight="auto"
         cols={1}
         spacing={8}
         style={{ width: "100%", marginBottom: "1rem" }}
       >
-        {items}
+        {Object.keys(items).map(key => {
+          const item = items[key]
+          return (
+            <GridListTile key={item.id} cols={1}>
+              <Item
+                name={item.name}
+                description={item.description}
+                owner={item.owner}
+                stars={item.stars}
+                forks={item.forks}
+                topics={item.topics}
+              />
+            </GridListTile>
+          )
+        })}
       </GridList>
     </Layout>
   )
